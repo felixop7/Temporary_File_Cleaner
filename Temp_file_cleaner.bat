f8eb56c34d27c577b822796dc3709a4037d78f83
Roshan Sahani <roshansahani226@gmail.com>
2023-07-24 08:16:27 +0545

    nirjalSAID

--- Temp_file_cleaner.bat
+++ Temp_file_cleaner.bat
@@ -5,11 +5,16 @@
 @echo.
 pause
 
+REM Add "cd /d %~dp0" to ensure the script's working directory is set to the script's location
+cd /d %~dp0
+
 color 3
-Del /S /F /Q %temp%
-Del /S /F /Q %Windir%\Temp
-Del /S /F /Q C:\WINDOWS\Prefetch
+REM Use double quotes around file paths to handle paths with spaces properly
+Del /S /F /Q "%temp%\*.*"
+Del /S /F /Q "%Windir%\Temp\*.*"
+Del /S /F /Q "C:\WINDOWS\Prefetch\*.*"
+
 @echo.
-@echo All Temporary Files Sucessfully Deleted!
+@echo All Temporary Files Successfully Deleted!
 @echo. 
 pause
